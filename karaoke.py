2dad4e53df854d5a0b6bf7b45653ef8a30e01666
auronff10 <ledesmax@gmail.com>
2014-10-09 21:03:13 +0200

    Karaoke.py modificado para ser orientado a objetos

--- karaoke.py
+++ karaoke.py
@@ -4,40 +4,50 @@ import sys
 from xml.sax import make_parser
 from smallsmilhandler import SmallSMILHandler
 import os
+
+
+class KaraokeLocal(SmallSMILHandler):
+
+    def __init__(self, fichero):
+
+        self.locales = []
+        parser = make_parser()
+        sHandler = SmallSMILHandler()
+        parser.setContentHandler(sHandler)
+        parser.parse(open(fichero))
+        self.lista = sHandler.get_tags()
+
+    def __str__(self):
+        for index in self.lista:
+            print index[0],
+            contenido = index[1]
+            for elemento in contenido:
+                if contenido[elemento] != "":
+                    print '\t', elemento, '=', contenido[elemento],
+            print
+
+    def do_local(self):
+        for index in self.lista:
+            contenido = index[1]
+            for elemento in contenido:
+                if contenido[elemento] != "":
+                    if elemento == "src":
+                        if contenido[elemento]:
+                            sources = contenido[elemento]
+                            os.system("wget -q " + sources)
+                            nombre = sources.split('/')
+                            nombre = nombre[-1]
+                            contenido[elemento] = nombre
+
+        print
+
 if __name__ == "__main__":
-    multimedia = []
+
     try:
         fichero = sys.argv[1]
     except IndexError:
         print 'Usage: python karaoke.py src_file.smil'
         sys.exit()
-    parser = make_parser()
-    sHandler = SmallSMILHandler()
-    parser.setContentHandler(sHandler)
-    parser.parse(open(fichero))
-    lista = sHandler.get_tags()
-    for linea in lista:
-        elemento = linea[0]
-        print elemento, (10 - len(elemento)) * " ",
-        atributos = str(linea[1])
-        atributos = atributos[1:-1]
-        atributos = atributos.split(",")
-        iteraciones = 0
-        for atributo in atributos:
-            iteraciones = iteraciones + 1
-            campos = atributo.split(": ")
-            clave = campos[0]
-            if iteraciones == 1:
-                clave = clave[1:-1]
-            else:
-                clave = clave[2:-1]
-                valor = campos[1]
-                valor = valor[1:-1]
-                sentencia = clave + "=" + '"' + valor + '"'
-                print ('\t'), sentencia,
-            print ('\n'),
-    for url in multimedia:
-        os.system("wget ­-q " + url)
-    locales = []
-    for valor in multimedia:
-        locales.append(valor.split("/")[-1])
+    KaraokeLocal(fichero).__str__()
+    KaraokeLocal(fichero).do_local()
+    KaraokeLocal(fichero).__str__()
